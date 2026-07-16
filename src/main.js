aface850df9714e9fc2f1da3864b43927b016064
zahrawjafari <zahra.jafari8112@gmail.com>
2026-07-16 22:06:28 +0330

    z

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 import './style.css'
+const menuBtn = document.getElementById("menuBtn");
+const closeMenu = document.getElementById("closeMenu");
+const mobileMenu = document.getElementById("mobileMenu");
 
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("hidden");
+});
+
+closeMenu.addEventListener("click", () => {
+  mobileMenu.classList.add("hidden");
+});
